81b4a556cdcf29821b81b41ff4d74f4c8c72ebee
Jatin Singhroha <my895012@gmail.com>
2021-05-22 19:59:32 +0530

    Constraint Layout implemented.

---
+++ app/src/main/java/com/jatinsinghroha/printstudentdetails/layoutexamples/ConstraintAdvancedExample.java
@@ -0,0 +1,16 @@
+package com.jatinsinghroha.printstudentdetails.layoutexamples;
+
+import android.os.Bundle;
+
+import com.jatinsinghroha.printstudentdetails.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConstraintAdvancedExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_advanced_example);
+    }
+}
